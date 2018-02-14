459d1b69698305ae5380506ecf223a6eb01d0532
Clément Boin <clem.boin@gmail.com>
2018-02-14 14:43:39 +0100

    add zsh

--- post-installation.sh
+++ post-installation.sh
@@ -2,7 +2,7 @@
 
 # Install packages
 echo "(i) install packages"
-sudo pacman -S - < pkglist.txt
+sudo -E pacman -S - < pkglist.txt
 
 # Set the keyboard layout settings
 echo "(i) set the keyboard layout"
@@ -21,8 +21,8 @@ ln -sf $dotfiles_dir/config_files/bash/bashrc $HOME/.bashrc
 echo "(i) set i3wm config"
 ln -sf $dotfiles_dir/config_files/i3wm/config $HOME/.config/i3/config 
 ln -sf $dotfiles_dir/config_files/i3wm/i3status.conf $HOME/.config/i3/i3status.conf 
-sudo wget http://static.simpledesktops.com/uploads/desktops/2015/02/20/zentree_1.png -O /usr/share/pixmaps/wallpaper.png
-sudo wget http://raspberrypi.windowswiki.info/img/archlinuxlogo.png -O /usr/share/pixmaps/arch-logo.png
+sudo -E wget http://static.simpledesktops.com/uploads/desktops/2015/02/20/zentree_1.png -O /usr/share/pixmaps/wallpaper.png
+sudo -E wget http://raspberrypi.windowswiki.info/img/archlinuxlogo.png -O /usr/share/pixmaps/arch-logo.png
 ln -sf $dotfiles_dir/config_files/i3wm/lock.sh $HOME/.config/i3/lock.sh
 sudo ln -sf $HOME/.config/i3/lock.sh /usr/bin/lock
 chmod +x /usr/bin/lock
@@ -44,3 +44,8 @@ ln -sf $dotfiles_dir/config_files/git/gitconfig $HOME/.gitconfig
 echo "(i) set vim config"
 ln -sf $dotfiles_dir/config_files/vim/vimrc $HOME/.vimrc
 ln -sf $dotfiles_dir/vim $HOME/.vim
+
+# Set zsh config
+echo "(i) set zsh config"
+ln -sf $dotfiles_dir/zsh/oh-my-zsh $HOME/.oh-my-zsh
+ln -sf $dotfiles_dir/zsh/zshrc $HOME/.zshrc
